75c0e74ecbbd449704ed9af6939de753c5bf082e
ThomasTadesse <328704@student.mboutrecht.nl>
2025-02-12 15:58:53 +0100

    controller && route

--- Query-and-SP/routes/web.php
+++ Query-and-SP/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HallController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/halls', [HallController::class, 'getHallsWithFacilities']);
